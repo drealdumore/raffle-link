516afe693b9e8fa7d08cb2d8e3e966255161afb0
drealdumore <sireroyce1@gmail.com>
2024-07-27 07:31:04 +0100

    update calender

--- lib/mongodb.ts
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-const client = new MongoClient(process.env.MONGODB_URI!, { useNewUrlParser: true, useUnifiedTopology: true });
-
-export async function connectToDatabase() {
-  if (!client.isConnected()) await client.connect();
-  return client.db();
-}
