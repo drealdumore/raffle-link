19f0f64751be77142ed25ac9154145ed1bfa3cc4
drealdumore <sireroyce1@gmail.com>
2024-07-29 01:28:19 +0100

    Added Spinner ✔

--- components/shared/nav.tsx
+++ components/shared/nav.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { auth, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
@@ -11,14 +9,11 @@ const Nav = async () => {
   return (
     <header className="container mx-auto flex max-w-5xl items-center justify-between px-4 py-2">
       <Link href="/">
-        <div className="md:text-xl select-none font-cal flex justify-center  gap-1 text-neutral-800">
+        <div className="md:text-xl select-none font-cal flex justify-center gap-1 text-neutral-800">
           <LinkIcon />
           <p>RaffleLink.</p>
         </div>
       </Link>
-
-      {/* TODO  - ADD THE CLERK USER ICON IF AUTHENTICATED OR CREATE MY OWN */}
-      {/* DO NOT SHOW THE CREATE RAFFLE WHEN PATHNAME === '/SIGN-IN' && '/SIGN-UP */}
 
       <nav className="flex gap-6">
         {!isAuth ? (
@@ -29,14 +24,11 @@ const Nav = async () => {
             Create Raffle
           </Link>
         ) : (
-          <>
-            <Link href="/profile">
-              <li>Profile</li>
-            </Link>
-            <li>
-              <UserButton afterSignOutUrl="/" />
-            </li>
-          </>
+          <div className="flex gap-2 items-center justify-center">
+            <Link href="/profile" className="hover:underline hover:underline-offset-2">Profile</Link>
+
+            <UserButton afterSignOutUrl="/" />
+          </div>
         )}
       </nav>
     </header>
@@ -44,45 +36,6 @@ const Nav = async () => {
 };
 
 export default Nav;
-
-function GithubIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="h-6 w-6 text-neutral-800 transition-colors hover:text-neutral-600 dark:text-neutral-200 dark:hover:text-neutral-400"
-    >
-      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
-      <path d="M9 18c-4.51 2-5-2-7-2"></path>
-    </svg>
-  );
-}
-
-function TwitterIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="h-6 w-6 text-neutral-800 transition-colors hover:text-neutral-600 dark:text-neutral-200 dark:hover:text-neutral-400"
-    >
-      <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
-    </svg>
-  );
-}
 
 function LinkIcon() {
   return (
--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,23 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
+  // public routes
   // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/api(.*)', '/_next/static(.*)', '/_next/image(.*)', '/favicon.ico', '/api/webhooks/clerk', '/raffle/new', '/profile'],
+
+  publicRoutes: [
+    "/",
+    "/sign-in",
+    "/sign-up",
+    "/api(.*)",
+    "/_next/static(.*)",
+    "/_next/image(.*)",
+    "/favicon.ico",
+    "/api/webhooks/clerk",
+  ],
 });
 
 export const config = {
   // Protect only specific routes
-  matcher: ['/raffle/new', '/profile'],
+  // Match all routes except API routes and Next.js internal routes
+  matcher: ["/((?!_next|api).*)"],
 };
-
-
-// import { authMiddleware } from "@clerk/nextjs";
-
-// export default authMiddleware({
-//   // Routes that can be accessed while signed out
-//   publicRoutes: ['/'],
-//   // Routes that can always be accessed, and have
-//   // no authentication information
-// //   ignoredRoutes: ['/no-auth-in-this-route'],
-// });
-
-// export const config = {
-//   // Protects all routes, including api/trpc.
-//   // See https://clerk.com/docs/references/nextjs/auth-middleware
-//   // for more information about configuring your Middleware
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-// import { authMiddleware } from "@clerk/nextjs";
-
-// export default authMiddleware({
-//   // Routes that can be accessed while signed out
-//   publicRoutes: ['/'],
-//   // Routes that can always be accessed, and have no authentication information
-//   // ignoredRoutes: ['/no-auth-in-this-route'],
-// });
-
-// export const config = {
-//   // Protect all routes except those matching the following patterns:
-//   matcher: [
-//     "/((?!api/.*|_next/static|_next/image|favicon.ico).*)", // Protect all routes except API and some static files
-//     "/", // Protect the root path
-//   ],
-// };
